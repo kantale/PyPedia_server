2bd6e2c35927cf01eff3f0531e9e09b563987c64
Kantale <alexandros.kanterakis@gmail.com>
2012-04-19 00:40:09 +0200

    Copyright year change

--- pypedia.php
+++ pypedia.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2009-2011 Alexandros Kanterakis
+ * Copyright (C) 2009-2012 Alexandros Kanterakis
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
