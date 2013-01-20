0ed5f6b74ce554e9e57e719becea9217eb7da224
root <root@snf-13900.(none)>
2013-01-20 03:19:21 +0200

    Added new version of MyVariables

--- MyVariables/MyVariables.php
+++ MyVariables/MyVariables.php
@@ -1,45 +1,43 @@
 <?php
-$wgCustomVariables = array('CURRENTUSER','CURRENTUSERREALNAME','LOGO');
- 
-$wgHooks['MagicWordMagicWords'][]          = 'wfAddCustomVariable';
-$wgHooks['MagicWordwgVariableIDs'][]       = 'wfAddCustomVariableID';
-$wgHooks['LanguageGetMagic'][]             = 'wfAddCustomVariableLang';
-$wgHooks['ParserGetVariableValueSwitch'][] = 'wfGetCustomVariable';
- 
-function wfAddCustomVariable(&$magicWords) {
-	foreach($GLOBALS['wgCustomVariables'] as $var) $magicWords[] = "MAG_$var";
-	return true;
-	}
- 
-function wfAddCustomVariableID(&$variables) {
-	foreach($GLOBALS['wgCustomVariables'] as $var) $variables[] = constant("MAG_$var");
-	return true;
-	}
- 
-function wfAddCustomVariableLang(&$langMagic, $langCode = 0) {
-	foreach($GLOBALS['wgCustomVariables'] as $var) {
-		$magic = "MAG_$var";
-		$langMagic[defined($magic) ? constant($magic) : $magic] = array(0,$var);
-		}
-	return true;
-	}
- 
-function wfGetCustomVariable(&$parser,&$cache,&$index,&$ret) {
-	switch ($index) {
- 
-		case MAG_CURRENTUSER:
-			$parser->disableCache(); # Mark this content as uncacheable
-			$ret = $GLOBALS['wgUser']->mName;
-			break;
- 
-		case MAG_LOGO:
-			$ret = $GLOBALS['wgLogo'];
-			break;
- 
-		case MAG_CURRENTUSERREALNAME:
-			$parser->disableCache(); # Mark this content as uncacheable
-			$ret = $GLOBALS['wgUser']->mRealName;
-			break;
-		}
-	return true;
-	}
+
+/**
+ * MyVariables - An extension that allows the addition of new built in variables
+ *
+ * @link https://www.mediawiki.org/wiki/Extension:MyVariables Documentation
+ * @file MyVariables.php
+ * @defgroup MyVariables
+ * @ingroup Extensions
+ * @author Aran Dunkley [http://www.organicdesign.co.nz/nad User:Nad]
+ * @author Pavel Astakhov <pastakhov@yandex.ru>
+ * @copyright © 2007 Aran Dunkley
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+// Check to see if we are being called as an extension or directly
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'This file is an extension to MediaWiki and thus not a valid entry point.' );
+}
+
+// Register this extension on Special:Version
+$wgExtensionCredits['parserhook'][] = array(
+	'path'           => __FILE__,
+	'name'           => 'MyVariables',
+	'version'        => '3.1.1',
+	'url'            => 'https://www.mediawiki.org/wiki/Extension:MyVariables',
+	'author'         => array( '[[mw:User:Nad|Aran Dunkley]]', '[[mw:User:Pastakhov|Pavel Astakhov]]', '...',  ),
+	'descriptionmsg' => 'myvariables-desc'
+);
+
+// Tell the whereabouts of files
+$dir = __DIR__;
+
+// Allow translations for this extension
+$wgExtensionMessagesFiles['MyVariables'] = $dir . '/MyVariables.i18n.php';
+$wgExtensionMessagesFiles['MyVariablesMagic'] = $dir . '/MyVariables.i18n.magic.php';
+
+//Preparing classes for autoloading
+$wgAutoloadClasses['MyVariables'] = $dir . '/MyVariables.body.php';
+
+// Register hooks
+$wgHooks['MagicWordwgVariableIDs'][] = 'MyVariables::declareVarIds';
+$wgHooks['ParserGetVariableValueSwitch'][] = 'MyVariables::assignAValue';
